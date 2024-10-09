963069fbe8258dab96d35f538789ca2f9ace839a
Kushal Niraula <kushal463334@gmail.com>
2024-10-09 12:07:00 +1000

    Create dto for user login and add an api call for login

--- Connectedness.API/Controllers/UsersController.cs
+++ Connectedness.API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Connectedness.API.Models;
 using Connectedness.API.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace Connectedness.API.Controllers
 {
@@ -13,6 +14,17 @@ namespace Connectedness.API.Controllers
         public UsersController(AppDbContext context) {
             _context = context; 
        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto dto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u=> u.Email == dto.Email);
+            if (user == null || user.PasswordHash != dto.Password) {
+                return Unauthorized("Invalid Email or Password! Please try again.");
+            }
+            return Ok(new {message = "Login successful!", userId = user.UserId});
+        }
+
         [HttpPost("register")]
         public IActionResult Register(UserRegisterDto dto)
         {
---
+++ Connectedness.API/DTOs/UserLoginDto.cs
@@ -0,0 +1,8 @@
+namespace Connectedness.API.DTOs
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
