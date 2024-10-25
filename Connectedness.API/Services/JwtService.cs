53e5cf2cf387095b7542f6255f41ce51bed0335f
Kushal Niraula <kushal463334@gmail.com>
2024-10-25 10:11:00 +1000

    Add JwtServices class and create a method to generate token for users

---
+++ Connectedness.API/Services/JwtService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Connectedness.API.Models;
+using System.Security.Cryptography;
+
+namespace Connectedness.API.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _config;
+        public JwtService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+            new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString() ),
+            new Claim(ClaimTypes.Name, user.FullName ),
+            new Claim(ClaimTypes.Email, user.Email )
+            };
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(7),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken( token );
+        }
+    }
+}
