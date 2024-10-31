a25d7750b7390f7d8410aed3f973a5b51970361b
Kushal Niraula <kushal463334@gmail.com>
2024-10-31 23:27:00 +1000

    Update Question Model to link with Group Model

--- Connectedness.API/Models/Question.cs
+++ Connectedness.API/Models/Question.cs
@@ -5,8 +5,13 @@ namespace Connectedness.API.Models
         public int QuestionId { get; set; }
         public required string Text { get; set; } 
         public int CreatedByUserId { get; set; }
-        public required User CreatedByUser { get; set; }
+        public User? CreatedByUser { get; set; }
         public required string CorrectAnswer { get; set; }
-        public required string[] Options { get; set; }
+        public required string IncorrectOption1 { get; set; }
+        public required string IncorrectOption2 { get; set; }
+        public required string IncorrectOption3 { get; set; }
+        public int GroupId { get; set; }
+        public Group? Group { get; set; }
+
     }
 }
