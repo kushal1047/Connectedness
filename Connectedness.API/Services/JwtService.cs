b84babaaf26ee54dde1680d8987b9ebf7063692d
Kushal Niraula <kushal463334@gmail.com>
2024-12-11 10:04:00 +1000

    configure api interceptor to refresh the token when access token expires

--- Connectedness.API/Services/JwtService.cs
+++ Connectedness.API/Services/JwtService.cs
@@ -30,7 +30,7 @@ namespace Connectedness.API.Services
             };
             var token = new JwtSecurityToken(
                 claims: claims,
-                expires: DateTime.UtcNow.AddDays(7),
+                expires: DateTime.UtcNow.AddMinutes(30),
                 signingCredentials: creds
                 );
             return new JwtSecurityTokenHandler().WriteToken( token );
--- connectedness-frontend/src/services/axios.js
+++ connectedness-frontend/src/services/axios.js
@@ -11,4 +11,27 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const res = await api.post("/auth/refresh-token", refreshToken);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+        originalRequest.headers.Authorization = `Bearer ${res.data.token}`;
+        return api(originalRequest);
+      } catch (err) {
+        console.error("Token refresh failed", err);
+        localStorage.clear();
+        window.location.href("/login");
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
